e1285577d0342752bd1e43bcd9fc965a796c4742
GPK <batman@Bruces-MacBook-Pro.local>
2018-12-31 04:21:03 +0000

    modification

--- agents/GBDTAgent.py
+++ agents/GBDTAgent.py
@@ -24,25 +24,27 @@ class GBDTAgent:
         self.explorationDiscountFactor = (0.01)**(1/400.) #0.994260074
         self.gamma = 0.95
         self.learningRate = 0.001
-        self.bufferSize = 1000000
+        self.bufferSize = 1000
         self.buffer = deque(maxlen=self.bufferSize)
-        self.batchSize = 30
+        self.batchSize = 20
         self.model = MultiOutputRegressor(LGBMRegressor(n_estimators=100, n_jobs=-1))
 
         self.ready = False
 
     # Return next action from agent
     def getNextAction(self, currentState, greedy = False):
-        currentState = np.array ([currentState])
-        if not self.ready or np.random.rand() <= self.explorationRate and not greedy:
+        if np.random.rand() < self.explorationRate and not greedy:
             return random.randrange(self.actionSpaceShape)
-        currentQ = self.model.predict(currentState)
+        if self.ready:
+            currentQ = self.model.predict(np.array([currentState]))
+        else:
+            currentQ = np.zeros(self.actionSpaceShape).reshape(1, -1)
         return np.argmax(currentQ[0])
 
     # Update policy of agent with sample returns
     def updatePolicy(self, currentState, lastAction, reward, done, successorState):
         # Save last state action pair to buffer
-        self.buffer.append((currentState, lastAction, reward, successorState, done))
+        self.buffer.append((currentState, lastAction, -reward if done else reward, successorState, done))
 
         # Update exploration rate at end of each epoch
         if done:
@@ -54,17 +56,52 @@ class GBDTAgent:
         if len(self.buffer) < self.batchSize:
             return
         batch = random.sample(self.buffer, self.batchSize)
-        for currentState, lastAction, reward, successorState, done in batch:
-            successorState = np.array([successorState])
-            currentState = np.array([currentState])
-            if not self.ready or done:
-                newQ_a = reward
+        
+        X = []
+        targets = []
+        for state, action, reward, state_next, terminal in batch:
+            state = np.array([successorState])
+            state_next = np.array([currentState])
+            q_update = reward
+            if not terminal:
+                if self.ready:
+                    q_update = (reward + self.gamma * np.amax(self.model.predict(state_next)))
+                    #print(self.model.predict(state_next))
+                else:
+                    q_update = reward
+            if self.ready:
+                q_values = self.model.predict(state)
             else:
-                newQ_a = (reward + self.gamma * np.amax(self.model.predict(successorState)))
-            newQ = self.model.predict(currentState) if self.ready else np.zeros(self.actionSpaceShape)
-            newQ[0][lastAction] = newQ_a
-            self.model.fit(currentState, newQ)
-            self.ready = True
+                q_values = np.zeros(self.actionSpaceShape).reshape(1, -1)
+            q_values[0][action] = q_update
+            
+            #print(state)
+            #print(action)
+            #print(q_values)
+            X.append(list(state[0]))
+            targets.append(q_values[0])
+        #print(X)
+        #print(targets)
+        self.model.fit(X, targets)
+        self.ready = True
+#         self.exploration_rate *= EXPLORATION_DECAY
+#         self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
+
+#         states = []
+#         targets = []
+#         for currentState, lastAction, reward, successorState, done in batch:
+#             if not self.ready or done:
+#                 newQ_a = reward
+#             else:
+#                 newQ_a = (reward + self.gamma * np.amax(self.model.predict(successorState)))
+#             newQ = self.model.predict(currentState) if self.ready else np.zeros(self.actionSpaceShape).reshape(1,-1)
+#             newQ[0][lastAction] = newQ_a
+            
+#             states.append(currentState)
+#             targets.append(newQ[0])
+
+#         self.model.fit(states, targets)
+#         self.ready = True
 
     # Save current policy model
     def saveModel(self, modelName ="kerasModel"):
